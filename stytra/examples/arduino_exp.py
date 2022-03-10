73107005c1a87cfee1d7aa7170670ac2f001c1e1
Benedetta Zattera <44493913+BZattera@users.noreply.github.com>
2022-03-10 15:41:41 +0100

    Update arduino_exp.py

--- stytra/examples/arduino_exp.py
+++ stytra/examples/arduino_exp.py
@@ -7,7 +7,7 @@ from lightparam import Param
 # applications you want to subclass the WriteArduinoPin class.
 class ArduinoProtocol(Protocol):
     name = 'arduino_protocol'
-    stytra_config = dict(arduino_config=dict(com_port="COM8", layout=[dict(pin=13, mode="pwm", ad="d"),dict(pin=5, mode="pwm", ad="d")]))
+    stytra_config = dict(arduino_config=dict(com_port="COM5", layout=[dict(pin=13, mode="pwm", ad="d"),dict(pin=5, mode="pwm", ad="d")]))
 
     def __init__(self):
         super(ArduinoProtocol, self).__init__()
